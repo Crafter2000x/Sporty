9666b1a1480e8259de54688279a4619771361d42
Thijs Vernooij <462752@student.fontys.nl>
2020-11-12 21:29:19 +0100

    BMI pagina veerbetering toegevoegd

--- Sporty/Sporty/Pages/BMIPage.xaml.cs
+++ Sporty/Sporty/Pages/BMIPage.xaml.cs
@@ -18,15 +18,21 @@ namespace Sporty
         }
         private void Button_Clicked(object sender, EventArgs e)
         {
-            double Gewicht = Convert.ToDouble(editgewicht.Text);
-            double lengte = Convert.ToDouble(editlengte.Text) / 100;
-            double lengtekwadraat = lengte * lengte;
+            try
+            {
+                double Gewicht = Convert.ToDouble(editgewicht.Text);
+                double lengte = Convert.ToDouble(editlengte.Text) / 100;
+                double lengtekwadraat = lengte * lengte;
 
-            double uitkomst = Gewicht / lengtekwadraat;
+                double uitkomst = Gewicht / lengtekwadraat;
 
 
-            txtBMI.Text = uitkomst.ToString();
-
+                txtBMI.Text = "Jouw BMI-waarde: " + uitkomst.ToString();
+            }
+            catch
+            {
+                txtBMI.Text = "Jouw BMI-waarde: 0";
+            }
 
         }
     }
