1d68d5283db8c147a5f02ddbd220de555d286a51
Connor Timmerman <457736@student.fontys.nl>
2020-11-16 15:49:20 +0100

    EINDELIJK KLAAR

--- Sporty/Sporty.Android/MainActivity.cs
+++ Sporty/Sporty.Android/MainActivity.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Android;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -12,6 +12,14 @@ namespace Sporty.Droid
     [Activity(Label = "Sporty", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -28,6 +36,38 @@ namespace Sporty.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == RequestLocationId)
+            {
+                if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted)) { }
+                // Permissions granted - display a message.
+                //else if()
+                //{ 
+                //    // Permissions denied - display a message.
+                //}
+                else
+                {
+                    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+                }
+            }
         }
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    RequestPermissions(LocationPermissions, RequestLocationId);
+                }
+                else
+                {
+                    // Permissions already granted - display a message.
+                }
+            }
+        }
+
+
     }
 }
--- Sporty/Sporty/Pages/WorkoutsPage.xaml.cs
+++ Sporty/Sporty/Pages/WorkoutsPage.xaml.cs
@@ -1,20 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace Sporty
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkoutsPage : ContentPage
     {
+        Stopwatch stopwatch;
         public WorkoutsPage()
         {
             InitializeComponent();
+            DisplayCurLoc();
+            stopwatch = new Stopwatch();
+        }
+
+        public async void DisplayCurLoc()
+        {
+            try
+            {
+                var requestStart = new GeolocationRequest(GeolocationAccuracy.High);
+                var locationBegin = await Geolocation.GetLocationAsync(requestStart);
+
+                if (locationBegin != null)
+                {
+                    Position Begin = new Position(locationBegin.Latitude, locationBegin.Longitude);
+                    MapSpan mapSpan = MapSpan.FromCenterAndRadius(Begin, Distance.FromMeters(.444));
+                    map.MoveToRegion(mapSpan);
+                }
+            }
+
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        string tijd;
+
+        private void btnStarten_Clicked(object sender, EventArgs e)
+        {
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+                Device.StartTimer(TimeSpan.FromMilliseconds(60), () =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                        lblStopwatch.Text = stopwatch.Elapsed.ToString()); // lblStopwatch.Text mogelijk veranderen door tijd
+                    if (!stopwatch.IsRunning)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                });
+            }
+        }
+
+        private void btnPauzeren_Clicked(object sender, EventArgs e)
+        {
+            btnStarten.Text = "Verder";
+            stopwatch.Stop();
+        }
+
+        public void btnStoppen_Clicked(object sender, EventArgs e)
+        {
+            double tijd = stopwatch.Elapsed.TotalMinutes;
+            tijd = Math.Round(tijd, 1);
+            txtTijd.Text = Convert.ToString(tijd);
+            stopwatch.Stop();
+            btnStarten.Text = "Starten";
+            lblStopwatch.Text = "00:00:00";
+        }
+
+        private void btnBereken_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SliderKeuze.Value.Equals(0))
+                {
+                    Bereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6);
+                    AlternatiefBereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 8, 6.4, "rennen", "fietsen");
+                }
+                else if (SliderKeuze.Value.Equals(1))
+                {
+                    Bereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 8);
+                    AlternatiefBereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6, 6.4, "lopen", "fietsen");
+                }
+                else if (SliderKeuze.Value.Equals(2))
+                {
+                    Bereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 6.4);
+                    AlternatiefBereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6, 8, "lopen", "rennen");
+                }
+            }
+            catch { }
+            stopwatch.Reset();
+        }
+
+        void Bereken(double Tijd, double Gewicht, double METWaarde)
+        {
+            double uitkomst;
+
+            uitkomst = Tijd * Gewicht * 0.0175 * METWaarde;
+            lblUitkomst.Text = "Aantal calorieen verbrand: " + Convert.ToInt32(uitkomst) + " kcal";
+        }
+
+        void AlternatiefBereken(double Tijd, double Gewicht, double METWaarde1, double METWaarde2, string Keuze1, string Keuze2)
+        {
+            double uitkomst1;
+            double uitkomst2;
+
+            uitkomst1 = Tijd * Gewicht * 0.0175 * METWaarde1;
+            uitkomst2 = Tijd * Gewicht * 0.0175 * METWaarde2;
+
+            lblAlternatief1.Text = " Bij " + Keuze1 + " had U " + Convert.ToInt32(uitkomst1) + " kcal verbrand.";
+            lblAlternatief2.Text = " Bij " + Keuze2 + " had U " + Convert.ToInt32(uitkomst2) + " kcal verbrand.";
+        }
+
+        private void btnLopen_Clicked(object sender, EventArgs e)
+        {
+            imgKeuze.Source = "Lopen.png";
+            SliderKeuze.Value = 0;
+            btnLopen.BackgroundColor = Color.Beige;
+            btnRennen.BackgroundColor = Color.WhiteSmoke;
+            btnFietsen.BackgroundColor = Color.WhiteSmoke;
+        }
+
+        private void btnRennen_Clicked(object sender, EventArgs e)
+        {
+            imgKeuze.Source = "Rennen.png";
+            SliderKeuze.Value = 1;
+            btnRennen.BackgroundColor = Color.Beige;
+            btnLopen.BackgroundColor = Color.WhiteSmoke;
+            btnFietsen.BackgroundColor = Color.WhiteSmoke;
+        }
+
+        private void btnFietsen_Clicked(object sender, EventArgs e)
+        {
+            imgKeuze.Source = "Fietsen.png";
+            SliderKeuze.Value = 2;
+            btnFietsen.BackgroundColor = Color.Beige;
+            btnRennen.BackgroundColor = Color.WhiteSmoke;
+            btnLopen.BackgroundColor = Color.WhiteSmoke;
         }
     }
 }
