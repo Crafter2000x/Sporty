e522860c80f4b18d4ba860f407aef07b88c40fae
Connor Timmerman <457736@student.fontys.nl>
2020-11-16 11:57:07 +0100

    Added BMI and Routes apge

--- Sporty/Sporty/Pages/BMIPage.xaml.cs
+++ Sporty/Sporty/Pages/BMIPage.xaml.cs
@@ -16,5 +16,44 @@ namespace Sporty
         {
             InitializeComponent();
         }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double Gewicht = Convert.ToDouble(editgewicht.Text);
+                double lengte = Convert.ToDouble(editlengte.Text) / 100;
+                double lengtekwadraat = lengte * lengte;
+
+                double uitkomst = Gewicht / lengtekwadraat;
+
+                if (uitkomst < 18.5)
+                {
+                    if (uitkomst == 0)
+                    {
+                        txtBMI.TextColor = Color.Black;
+                    }
+                    else
+                    {
+                        txtBMI.TextColor = Color.Orange;
+                    }
+
+                }
+                else if (uitkomst >= 18.5 && uitkomst < 24.5)
+                {
+                    txtBMI.TextColor = Color.LimeGreen;
+                }
+                else if (uitkomst >= 24.5)
+                {
+                    txtBMI.TextColor = Color.Red;
+                }
+                uitkomst = Math.Round(uitkomst, 1);
+                txtBMI.Text = "Jouw BMI-waarde: " + uitkomst.ToString();
+            }
+            catch
+            {
+                txtBMI.Text = "Jouw BMI-waarde: 0";
+            }
+
+        }
     }
 }
