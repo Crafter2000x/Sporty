bf4b7f273ff281d337f707df27a952864a5d5176
SiebeSijbers <siebesijbers@gmail.com>
2020-11-16 15:12:12 +0100

    Workout page works finally

--- Sporty/Sporty.Android/MainActivity.cs
+++ Sporty/Sporty.Android/MainActivity.cs
@@ -6,18 +6,28 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android;
 
 namespace Sporty.Droid
 {
     [Activity(Label = "Sporty", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -28,6 +38,37 @@ namespace Sporty.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == RequestLocationId)
+            {
+                if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted)) { }
+                // Permissions granted - display a message.
+                //else if()
+                //{ 
+                //    // Permissions denied - display a message.
+                //}
+                else
+                {
+                    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+                }
+            }
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    RequestPermissions(LocationPermissions, RequestLocationId);
+                }
+                else
+                {
+                    // Permissions already granted - display a message.
+                }
+            }
         }
     }
 }
--- Sporty/Sporty/Pages/WorkoutsPage.xaml.cs
+++ Sporty/Sporty/Pages/WorkoutsPage.xaml.cs
@@ -1,20 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace Sporty
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkoutsPage : ContentPage
     {
+        Stopwatch stopwatch;
         public WorkoutsPage()
         {
             InitializeComponent();
+            DisplayCurLoc();
+            stopwatch = new Stopwatch();
+        }
+
+        public async void DisplayCurLoc()
+        {
+            try
+            {
+                var requestStart = new GeolocationRequest(GeolocationAccuracy.High);
+                var locationBegin = await Geolocation.GetLocationAsync(requestStart);
+
+                if (locationBegin != null)
+                {
+                    Position Begin = new Position(locationBegin.Latitude, locationBegin.Longitude);
+                    MapSpan mapSpan = MapSpan.FromCenterAndRadius(Begin, Distance.FromMeters(.444));
+                    map.MoveToRegion(mapSpan);
+                }
+            }
+
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        string tijd;
+
+        private void btnStarten_Clicked(object sender, EventArgs e)
+        {
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+                Device.StartTimer(TimeSpan.FromMilliseconds(60), () =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                        lblStopwatch.Text = stopwatch.Elapsed.ToString()); // lblStopwatch.Text mogelijk veranderen door tijd
+                    if (!stopwatch.IsRunning)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                });
+            }
+        }
+
+        private void btnPauzeren_Clicked(object sender, EventArgs e)
+        {
+            btnStarten.Text = "Verder";
+            stopwatch.Stop();
+        }
+
+        public void btnStoppen_Clicked(object sender, EventArgs e)
+        {
+            double tijd = stopwatch.Elapsed.TotalMinutes;
+            tijd = Math.Round(tijd, 1);
+            txtTijd.Text = Convert.ToString(tijd);
+            stopwatch.Stop();
+            btnStarten.Text = "Starten";
+            lblStopwatch.Text = "00:00:00";
         }
 
         private void btnBereken_Clicked(object sender, EventArgs e)
@@ -23,23 +100,25 @@ namespace Sporty
             {
                 if (SliderKeuze.Value.Equals(0))
                 {
-                    Bereken(Convert.ToInt32(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6);
-                    AlternatiefBereken(Convert.ToInt32(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 8, 6.4, "rennen", "fietsen");
+                    Bereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6);
+                    AlternatiefBereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 8, 6.4, "rennen", "fietsen");
                 }
                 else if (SliderKeuze.Value.Equals(1))
                 {
-                    Bereken(Convert.ToInt32(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 8);
-                    AlternatiefBereken(Convert.ToInt32(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6, 6.4, "lopen", "fietsen");
+                    Bereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 8);
+                    AlternatiefBereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6, 6.4, "lopen", "fietsen");
                 }
                 else if (SliderKeuze.Value.Equals(2))
                 {
-                    Bereken(Convert.ToInt32(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 6.4);
-                    AlternatiefBereken(Convert.ToInt32(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6, 8, "lopen", "rennen");
+                    Bereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 6.4);
+                    AlternatiefBereken(Convert.ToDouble(txtTijd.Text), Convert.ToDouble(txtGewicht.Text), 3.6, 8, "lopen", "rennen");
                 }
-            } catch { }
+            }
+            catch { }
+            stopwatch.Reset();
         }
 
-        void Bereken(int Tijd, double Gewicht, double METWaarde)
+        void Bereken(double Tijd, double Gewicht, double METWaarde)
         {
             double uitkomst;
 
@@ -47,7 +126,7 @@ namespace Sporty
             lblUitkomst.Text = "Aantal calorieen verbrand: " + Convert.ToInt32(uitkomst) + " kcal";
         }
 
-        void AlternatiefBereken(int Tijd, double Gewicht, double METWaarde1, double METWaarde2, string Keuze1, string Keuze2)
+        void AlternatiefBereken(double Tijd, double Gewicht, double METWaarde1, double METWaarde2, string Keuze1, string Keuze2)
         {
             double uitkomst1;
             double uitkomst2;
@@ -55,8 +134,8 @@ namespace Sporty
             uitkomst1 = Tijd * Gewicht * 0.0175 * METWaarde1;
             uitkomst2 = Tijd * Gewicht * 0.0175 * METWaarde2;
 
-            lblAlternatief1.Text = "   Bij " + Keuze1 + " had U " + Convert.ToInt32(uitkomst1) + " kcal verbrand.";
-            lblAlternatief2.Text = "   Bij " + Keuze2 + " had U " + Convert.ToInt32(uitkomst2) + " kcal verbrand.";
+            lblAlternatief1.Text = " Bij " + Keuze1 + " had U " + Convert.ToInt32(uitkomst1) + " kcal verbrand.";
+            lblAlternatief2.Text = " Bij " + Keuze2 + " had U " + Convert.ToInt32(uitkomst2) + " kcal verbrand.";
         }
 
         private void btnLopen_Clicked(object sender, EventArgs e)
